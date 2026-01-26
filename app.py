82d0ce898fc034111c038ae42b9fb76dcdc5ae7f
h <harshithk.kohli@gmail.com>
2026-01-26 19:23:31 +1100

    Expense Listing
    - showcasing all the expenses incurred on the website.
    
    - Sorted by descending date and category.
    
    - Added a delete button for each added expense.

--- app.py
+++ app.py
@@ -26,10 +26,13 @@ with app.app_context():
 
 
 
-
 @app.route("/")
 def index():
-    return render_template("index.html")
+
+    expenses = Expense.query.order_by(Expense.date.desc(), Expense.id.desc()).all()
+    return render_template("index.html", expenses=expenses)
+
+
 
 @app.route("/add", methods=['POST'])
 def add():
@@ -39,11 +42,12 @@ def add():
     category = (request.form.get("category") or "").strip()
     date_str = (request.form.get("date") or "").strip()
 
+    #ensures description, amount and category are listed.
     if not description or not amount_str or not category:
         flash("Please fill desciption, amount, and category", "error")
         return redirect(url_for("index"))
 
-
+    #converts the amount input string into an actual number
     try:
         amount = float(amount_str)
         if amount <= 0:
@@ -54,7 +58,7 @@ def add():
         return redirect(url_for("index"))
     
 
-
+    #converts the date into storable format
     try:
         d = datetime.strptime(date_str, "%Y-%m-%d").date() if date_str else date.today()
 
@@ -62,7 +66,7 @@ def add():
         d = date.today()
 
 
-
+    #saving to the database
     e = Expense(description=description, amount=amount, category=category, date=d)
     db.session.add(e)
     db.session.commit()
